0aaba3bd68476db328b423f108949d25538b7e13
dk949 <56653556+dk949@users.noreply.github.com>
2023-10-17 23:28:44 +0100

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,17 @@
 #include <project/config.hpp>
 
 int main() {
-    fmt::print("hello world\n");
+    fmt::print("{}: {} \n"
+               "\n"
+               "version v{}\n"
+               "\n"
+               "Try editing the source code in {}\n"
+               "\n"
+               "Find more info at {}/wiki\n"
+               "\n",
+        project::name,
+        project::description,
+        project::version::full,
+        project::dirs::src,
+        project::url);
 }
