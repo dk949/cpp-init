31411075fe54b71a23a64f958c45d2b08ecacc09
dk949 <56653556+dk949@users.noreply.github.com>
2023-10-22 01:02:02 +0100

    added no-error for unused warnings

--- cmake/compiler_warnings.cmake
+++ cmake/compiler_warnings.cmake
@@ -29,7 +29,7 @@ function (set_target_warnings project_name)
         /permissive- # standards conformance mode for MSVC compiler.
     )
 
-    set(CLANG_WARNINGS
+    set(COMMON_WARNINGS
         -Wall
         -Wextra # reasonable and standard
         -Wshadow # warn the user if a variable declaration shadows one from a parent context
@@ -45,20 +45,36 @@ function (set_target_warnings project_name)
         -Wnull-dereference # warn if a null dereference is detected
         -Wdouble-promotion # warn if float is implicit promoted to double
         -Wformat=2 # warn on security issues around functions that format output (ie printf)
+
+        # Warnings which are not errors even when -Werror is on
+        -Wno-error=unused-parameter
+        -Wno-error=unused-but-set-parameter
+        -Wno-error=unused-variable
+        -Wno-error=unused-but-set-variable
+        -Wno-error=unused-const-variable
+        -Wno-error=unused-function
+        -Wno-error=unused-label
+        -Wno-error=unused-local-typedefs
+        -Wno-error=unused-value
     )
 
     if (WARNINGS_AS_ERRORS)
-        set(CLANG_WARNINGS ${CLANG_WARNINGS} -Werror)
+        set(COMMON_WARNINGS ${COMMON_WARNINGS} -Werror)
         set(MSVC_WARNINGS ${MSVC_WARNINGS} /WX)
     endif ()
 
     set(GCC_WARNINGS
-        ${CLANG_WARNINGS}
+        ${COMMON_WARNINGS}
         -Wmisleading-indentation # warn if indentation implies blocks where blocks do not exist
         -Wduplicated-cond # warn if if / else chain has duplicated conditions
         -Wduplicated-branches # warn if if / else branches have duplicated code
         -Wlogical-op # warn about logical operations being used where bitwise were probably wanted
         -Wuseless-cast # warn if you perform a cast to the same type
+    )
+
+    set(CLANG_WARNINGS
+        ${COMMON_WARNINGS}
+        -Wno-error=unused-private-field
     )
 
     if (MSVC)
