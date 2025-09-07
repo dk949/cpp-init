d6601b97a587e7a900b7ea69bd05e134df48df3f
dk949 <56653556+dk949@users.noreply.github.com>
2025-09-08 00:17:16 +0100

    removed fix for clang14, shouldn't be needed anymore

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,7 @@
 #include <fmt/format.h>
 #include <project/config.hpp>
-#if defined(CPP_INIT_COMPILER_Clang) && CPP_INIT_COMPILER_VERSION_MAJOR <= 14
-#    include <experimental/source_location>
-#    define CPP_INIT_STD_EXPERIMENTAL std::experimental
-#else
-#    include <source_location>
-#    define CPP_INIT_STD_EXPERIMENTAL std
-#endif
+
+#include <source_location>
 
 int main() {
     fmt::print(R"({}: {}
@@ -15,7 +10,7 @@ version v{}
 
 Compiling on {} {} with {} {} compiler (using {})
 
-Try editing the source code in ./{}
+Try editing the source code in {}
 
 Or adding a test in ./tests
 
@@ -30,7 +25,6 @@ Find more info at {}/wiki
         cpp_init::compiler::vendor_str,
         cpp_init::compiler::version::full,
         cpp_init::lang::std_str,
-        // Clang <= 14 supporrt
-        CPP_INIT_STD_EXPERIMENTAL::source_location::current().file_name(),
+        std::source_location::current().file_name(),
         cpp_init::url);
 }
