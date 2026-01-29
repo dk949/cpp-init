f3535c26662c32bbf12ee28a2c96841ea023f74b
dk949 <56653556+dk949@users.noreply.github.com>
2026-01-29 01:44:07 +0000

    gcc<14 doesn't have <print> 🙁

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,38 @@
 #include "factorial.hpp"
 
+#include <%%cpp_init_replace%%/config.hpp>
 #include <%%cpp_init_replace%%/fib.hpp>
 
-#include <print>
+#include <cstdlib>
+#if defined(%%CPP_INIT_REPLACE%%_COMPILER_GNU) && %%CPP_INIT_REPLACE%%_COMPILER_VERSION_MAJOR < 14
+#    include <cstdio>
+#    include <format>
+#    define %%CPP_INIT_REPLACE%%_PRINTLN(fp, ...)                       \
+        (void)std::fputs(std::format(__VA_ARGS__).c_str(), fp); \
+        (void)std::fputc('\n', fp)
+#else
+#    include <print>
+#    define %%CPP_INIT_REPLACE%%_PRINTLN(...) std::println(__VA_ARGS__)
+#endif
 #include <stdexcept>
 #include <string>
-#include <cstdlib>
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        std::println(stderr, "Wrong number of arguments, expected 1, got {}", argc - 1);
+        %%CPP_INIT_REPLACE%%_PRINTLN(stderr, "Wrong number of arguments, expected 1, got {}", argc - 1);
         return 1;
     }
     auto const num = [arg = argv[1]]() {
         try {
             return std::stoll(arg);
         } catch (std::logic_error const &e) {
-            std::println(stderr, "Could not parse command line argument: {}", e.what());
+            %%CPP_INIT_REPLACE%%_PRINTLN(stderr, "Could not parse command line argument: {}", e.what());
             std::exit(1);  // NOLINT(concurrency-mt-unsafe)
         }
     }();
 
-    std::println("{}! = {}", num, %%cpp_init_namespace%%::factorial(num));
-    std::println("fib({}) = {}", num, %%cpp_init_namespace%%::fib(num));
+    %%CPP_INIT_REPLACE%%_PRINTLN(stdout, "{}! = {}", num, %%cpp_init_namespace%%::factorial(num));
+    %%CPP_INIT_REPLACE%%_PRINTLN(stdout, "fib({}) = {}", num, %%cpp_init_namespace%%::fib(num));
 
     return 0;
 }
